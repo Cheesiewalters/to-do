5df7b1a8c7d98bd563521b81f431b918f25cbbc6
cheesiewalters <cwalters01@qub.ac.uk>
2021-09-21 23:05:39 +0100

    Initial implelemtation of basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const inputStyle = {
+  width: 235,
+  margin: 5,
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      input: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+    this.buttonToComplete = this.buttonToComplete.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState(() => {
+      return {
+        input: e.target.value,
+      };
+    });
+  }
+
+  submit() {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        this.state.input,
+        // this.state.todos.markedAsComplete,
+      ],
+    });
+  }
+
+  buttonToComplete(td) {
+    let filteredArray = this.state.todos.filter((item) => item !== td);
+    this.setState({ todos: filteredArray });
+  }
+
+  render() {
+    const todos = this.state.todos.map((td) => {
+      return (
+        <ul>
+          <li>
+            {td}
+            <button
+              className="mark-complete-button"
+              onClick={() => this.buttonToComplete(td)}
+            >
+              Mark as Complete
+            </button>
+          </li>
+        </ul>
+      );
+    });
+    return (
+      <div className="body-container">
+        <div className="input-container">
+          <input
+            style={inputStyle}
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <button onClick={this.submit} className="button">
+          Create todo!
+        </button>
+        <div className="todo-container-outer">
+          {this.state.todos.length > 0 ? (
+            <div className="todo-container-inner">{todos}</div>
+          ) : (
+            <div>No to-do's in list</div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
