a14df5a681178064b6e0d30768af49b1a99ea080
cheesiewalters <cwalters01@qub.ac.uk>
2021-09-22 15:05:19 +0100

    Iniital Styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { Icon } from "@iconify/react";
 import React from "react";
-
+import locationIcon from "@iconify/icons-mdi/check-circle";
 const inputStyle = {
   width: 235,
   margin: 5,
@@ -27,41 +28,46 @@ class App extends React.Component {
 
   submit() {
     this.setState({
-      todos: [
-        ...this.state.todos,
-        this.state.input,
-        // this.state.todos.markedAsComplete,
-      ],
+      todos: [...this.state.todos, this.state.input],
     });
   }
 
+  //
+  //When the user clicks the markasComplete button this function is called
+  //It splices the state todo array to remove the index passed into the function from the array
+  //This spliced array is then set to a temp array which is used to update state to the smaller array
+  //
   buttonToComplete(td) {
-    let filteredArray = this.state.todos.filter((item) => item !== td);
+    let beforeArr = this.state.todos;
+    console.log("before splice" + beforeArr);
+    this.state.todos.splice(td, 1);
+    let filteredArray = this.state.todos;
+    console.log("after splice" + filteredArray);
     this.setState({ todos: filteredArray });
   }
 
   render() {
-    const todos = this.state.todos.map((td) => {
+    const todos = this.state.todos.map((td, index) => {
       return (
-        <ul>
-          <li>
-            {td}
-            <button
-              className="mark-complete-button"
-              onClick={() => this.buttonToComplete(td)}
-            >
-              Mark as Complete
-            </button>
-          </li>
-        </ul>
+        <li className="list-items">
+          {td}
+          <button
+            className="mark-complete-button"
+            onClick={() => this.buttonToComplete(index)}
+          >
+            <Icon icon={locationIcon} className="complete-icon"></Icon>
+          </button>
+        </li>
       );
     });
     return (
       <div className="body-container">
+        <h1 className="header-text-container">Conor's Todo Tracker </h1>
         <div className="input-container">
           <input
             style={inputStyle}
             value={this.state.input}
+            placeholder="Enter your todo here"
             onChange={this.handleChange}
           />
         </div>
